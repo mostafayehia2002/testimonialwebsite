cf611fbeec8300c1e1f90b65765db8e34e943000
mostafa yehia <96825436+mostafayehia2002@users.noreply.github.com>
2022-07-31 15:45:29 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+function method(){
+    var x=document.getElementById("no1");
+    if(x.style.display=="none")
+     {
+           x.style.display="block";
+    }
+     else{
+     x.style.display="none";
+    } 
+}
